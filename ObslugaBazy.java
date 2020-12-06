7d81b7999f486ba788adc148232481523132bc6f
Dkr0l <46425990+Dkr0l@users.noreply.github.com>
2020-12-06 22:16:49 +0100

    Update ObslugaBazy.java

--- ObslugaBazy.java
+++ ObslugaBazy.java
@@ -19,7 +19,7 @@ public class ObslugaBazy {
       stmt = conn.createStatement();
       String sql;
 	   
-      sql="CREATE TABLE Users (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,firstname VARCHAR(30) NOT NULL,lastname VARCHAR(30) NOT NULL,email VARCHAR(50),reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";	
+      sql="CREATE TABLE Users (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,firstname VARCHAR(30) NOT NULL,lastname VARCHAR(30) NOT NULL,email VARCHAR(50))";	
       stmt.executeUpdate(sql);
       System.out.println("Created table in given database...");
 	   
